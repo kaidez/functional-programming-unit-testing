0d60c26b10d85c268dbb01c0e0173ed4571e0fde
Kai Gittens <kai.gittens@gmail.com>
2017-02-25 06:03:52 -0500

    add "log()" and "doSomthing()" code...comment out "addMagic()" code for now

--- build/static/scripts.js
+++ build/static/scripts.js
@@ -1,1 +1,12 @@
-addMagic("section", "target", "foo");
+var myContainer = "Hey everybody! Come see how good I look!";
+log(myContainer);
+
+
+var classyMessage = function() {
+    return console.log("Stay classy San Diego!");
+}
+
+doSomething(classyMessage);
+
+
+//addMagic("section", "target", "foo");
