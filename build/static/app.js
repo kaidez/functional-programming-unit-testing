af72ef04edf5a2022d989642d4370feedf9bdc37
Kai Gittens <kai.gittens@gmail.com>
2017-02-25 06:00:28 -0500

    update whitespace, rearrange code

--- build/static/app.js
+++ build/static/app.js
@@ -14,13 +14,13 @@ var log = function(someVariable) {
 };
 
 var doSomething = function(thing) {
-    return thing();
+  return thing();
 }
 
 function addMagic(el, target, text, className) {
   var setElement = document.createElement(el);
+  var findElement = document.getElementById(target);
   setElement.innerHTML = text;
   setElement.setAttribute("class", className);
-  var findElement = document.getElementById(target);
   findElement.appendChild(setElement);
 };
