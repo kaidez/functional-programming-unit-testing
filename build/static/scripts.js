fd6d75f50a5a5cbc2116c2299e016d10d5a3c6bb
Kai Gittens <kai.gittens@gmail.com>
2017-02-27 08:00:52 -0500

    remove old code and invoke buildArticles()

--- build/static/scripts.js
+++ build/static/scripts.js
@@ -1,18 +1,1 @@
-var myContainer = "Hey everybody! Come see how good I look!";
-log(myContainer);
-
-
-var sayBigDeal = function() {
-    var message = "I’m kind of a big deal";
-    log(message);
-}
-
-doSomething(sayBigDeal);
-
-var someCarousel = new Carousel("carousel-one", 5435);
-someCarousel.init();
-
-
-addMagic("unicorn", "spin");
-addMagic("fairy", "sparkle");
-addMagic("kitten", "rainbow");
+buildArticles(buildList([1, 2, 3],"target"));
