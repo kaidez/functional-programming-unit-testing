c9dd9a53e33e88a471d9dec3ed36f6253b391cfd
Kai Gittens <kai.gittens@gmail.com>
2017-02-26 19:50:30 -0500

    start over by removing old code and creating a new "buildArticles()" function

--- build/static/app.js
+++ build/static/app.js
@@ -1,40 +1,5 @@
 "use strict";
 
-var log = function(someVariable) {
-  if((typeof someVariable != "string") || (someVariable.length <= 0)) {
-    throw new Error("expecting a string with at least one character");
-  } else {
-    console.log(someVariable);
-    return someVariable;
-  }
-};
-
-var doSomething = function(someFunction) {
+var buildArticles = function(someFunction) {
   return someFunction();
-}
-
-/* Pass undefined as the last parameter to prevent extra params being
- * passed...old skool Paul Irish hack: http://bit.ly/2mpRQ4o
- */
-function Carousel(getElement, spinDuration, undefined) {
-  this.getElement = getElement;
-  this.spinDuration = spinDuration;
-
-  if((typeof getElement != "string") || (typeof spinDuration != "number") || (arguments.length != 2)) {
-    throw new Error("Carousel() takes exactly 2 parameters: the first one (getElement) must be a string, the second one (spinDuration) must be a number");
-  } else {
-    return this;
-  }
-}
-
-Carousel.prototype.init = function(){
-  var getSlider = document.getElementById(this.getElement);
-  return getSlider.innerHTML = "The slider's working...";
-};
-
-function addMagic(id, effect) {
-  var element = document.getElementById(id);
-  element.className += '-magic';
-  element.innerHTML = effect;
-  console.log(element);
 }
