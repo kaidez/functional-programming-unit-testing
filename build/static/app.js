e0e1618fb7a72620a98772015c42a2a5e03eaab7
Kai Gittens <kai.gittens@gmail.com>
2017-02-27 07:47:44 -0500

    update app.js (see message) -do function type checking inside of "buildArticles()" -convert "buildList()" to a function expression -use getElementById() to find the "targetElement" parameter -use a forEach() to build the "listItem" -move "listItem" variable inside the forEach() loop

--- build/static/app.js
+++ build/static/app.js
@@ -1,11 +1,22 @@
 "use strict";
 
 var buildArticles = function(someFunction) {
-  return someFunction();
+  if(typeof someFunction != "function") {
+    return typeof someFunction == "function";
+  }
 }
 
-function buildList(getArray){
-  var list = document.createElement("ul"),
-      listItem = document.createElement("li");
+var buildList = function(getArray, targetElement) {
+
+  var getTarget = document.getElementById(targetElement),
+      list = document.createElement("ul");
+
+  getArray.forEach(function(i){
+    var listItem = document.createElement("li");
+    listItem.innerHTML = i;
+    list.appendChild(listItem);
+  });
+
+  return getTarget.appendChild(list);
 
 }
