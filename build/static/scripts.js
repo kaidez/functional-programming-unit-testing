f6642b15d8865894af967e597a4c5a41238e8294
Kai Gittens <kai.gittens@gmail.com>
2017-02-28 22:18:39 -0500

    remove console.log statement

--- build/static/scripts.js
+++ build/static/scripts.js
@@ -2,7 +2,7 @@ var teams = buildList(["Chelsea FC", "Real Madrid", "Paris St-Germain"],"target"
 
 var teams2 = ["Arsenal", "Sevilla", "Olympique de Marseille"];
 var teams2set = buildList(teams2, "target");
-console.log(typeof teams2set);
+
 placeList(teams2set);
 
 placeList(buildList(["Manchester United", "FC Barcelona", "AC Monaco"],"target"));
