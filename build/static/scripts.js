198c85656a2f274bed7106de07477041b79cc5a0
Kai Gittens <kai.gittens@gmail.com>
2017-03-08 06:26:37 -0500

    update "scripts.js" (see message) -remove "myContainer" variable -update how "log()" is invoked...move it down the page -add "sayBigDeal()" and have it run "log()" internally -remove "addMagic()" invocations -invoke "initialiseCarousel()"

--- build/static/scripts.js
+++ build/static/scripts.js
@@ -1,18 +1,12 @@
-var myContainer = "Hey everybody! Come see how good I look!";
-log(myContainer);
-
-
 var sayBigDeal = function() {
-    var message = "I’m kind of a big deal";
-    log(message);
+  var message = "I’m kind of a big deal";
+  log(message);
 }
 
+log("I’m kind of a big deal");
 doSomething(sayBigDeal);
 
 var someCarousel = new Carousel("carousel-one", 5435);
 someCarousel.init();
 
-
-addMagic("unicorn", "spin");
-addMagic("fairy", "sparkle");
-addMagic("kitten", "rainbow");
+initialiseCarousel('kitten', 3000);
