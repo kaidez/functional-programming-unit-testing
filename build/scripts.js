25076302e5206bbb4cb9de72e4f8e41b7776120d
Kai Gittens <kai.gittens@gmail.com>
2017-03-14 14:22:58 -0400

    remove uneeded console.statements

--- build/scripts.js
+++ build/scripts.js
@@ -15,8 +15,6 @@ var someCarousel = new Carousel('carousel-one', 5435);
 someCarousel.init();
 
 var testCarousel = initialiseCarousel('main-carousel', 3000);
-console.log(testCarousel instanceof Carousel);
-console.log(testCarousel.constructor);
 
 addMagic('unicorn', spin);
 addMagic('fairy', sparkle);
