934f703f9ef8d7149931f990cc1233bbf22f06af
Kai Gittens <kai.gittens@gmail.com>
2017-03-14 00:06:22 -0400

    add scripts.js(see message) -change double quotes to single quotes -change unicode single quotes to regular ones

--- build/scripts.js
+++ build/scripts.js
@@ -1,15 +1,15 @@
 var sayBigDeal = function() {
-  var message = "I’m kind of a big deal";
+  var message = "I'm kind of a big deal";
   log(message);
 }
 
-log("I’m kind of a big deal");
+log("I'm kind of a big deal");
 
 doSomething(sayBigDeal);
 
 initialiseCarousel('kitten', 3000);
 
-var someCarousel = new Carousel("carousel-one", 5435);
+var someCarousel = new Carousel('carousel-one', 5435);
 someCarousel.init();
 
 var testCarousel = initialiseCarousel('main-carousel', 3000);
