51fda568dca5cbc1e6e7b2662787f3cb079a15d3
Kai Gittens <kai.gittens@gmail.com>
2017-03-19 18:36:30 -0400

    create "build-complete/" folder

---
+++ build-complete/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var log = function(someVariable) {
+  if((typeof someVariable !== 'string') || (someVariable.length <= 0)) {
+    throw new Error('expecting a string with at least one character');
+  } else {
+    console.log(someVariable);
+    return someVariable;
+  }
+};
+
+var doSomething = function(someFunction) {
+  if(!$.isFunction(someFunction)) {
+    throw new Error("doSomething's parameter must be a function");
+  } else {
+    return someFunction();
+  }
+};
+
+function Carousel(getElement, spinDuration) {
+  this.getElement = getElement;
+  this.spinDuration = spinDuration || 3000;
+  if(this.getElement === undefined) {
+    throw new Error('Carousel needs to know what element to load into');
+  } else {
+    return this;
+  }
+}
+
+Carousel.prototype.init = function() {
+  var getCarousel = document.getElementById(this.getElement);
+  getCarousel.innerHTML = 'The ' + this.getElement + ' carousel has started.';
+};
+
+function initialiseCarousel(id, frequency) {
+  var slider = new Carousel(id, frequency);
+  slider.init();
+  return slider;
+}
+
+function addMagic(id, effect) {
+  if(!id || !effect) {
+    throw new Error('addMagic() needs an id and effect parameter');
+  } else {
+    var element = document.getElementById(id);
+    element.className += ' magic';
+    return effect(element);
+  }
+}
+
+function spin(getElement){
+  getElement.innerHTML = 'spinning...';
+}
+
+function sparkle(getElement){
+  getElement.innerHTML = 'sparkling...';
+}
+
+function rainbow(getElement){
+  getElement.innerHTML = 'rainbowing...';
+}
---
+++ build-complete/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+log("I'm kind of a big deal");
+
+var sayBigDeal = function() {
+  var message = "I'm kind of a big deal";
+  log(message);
+}
+
+doSomething(sayBigDeal);
+
+initialiseCarousel('kitten', 3000);
+
+var someCarousel = new Carousel('carousel-one', 5435);
+someCarousel.init();
+
+var someOtherCarousel = new Carousel('carousel-two');
+someOtherCarousel.init();
+
+var testCarousel = initialiseCarousel('main-carousel', 3000);
+
+addMagic('unicorn', spin);
+addMagic('fairy', sparkle);
+addMagic('kitten', rainbow);
